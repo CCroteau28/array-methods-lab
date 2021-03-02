09031fa3548db64687777b0ab799074672d51b78
CCroteau28 <ccroteau28@gmail.com>
2021-03-01 17:16:09 -0700

    commit tree 0d4d5609dd661366b88a81e94c7eb9fe2636ca68

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+words = ["wow", "I"]
+words.sort
 end
 
 def using_reverse(array)
-
+element = ["wow", "arrays!"]
+element.reverse
 end
 
 def using_first(array)
-
+word = ["wow", "arrays!"]
+word.first
 end
 
 def using_last(array)
-
+element = ["wow", "arrays!"]
+element.last
 end
 
 def using_size(array)
-
+  words = ["wow", "I", "am", "really", "learning", "arrays!"]
+  words.size
 end
